19354a42fed4e5109dbc878f7ee8a3d95296649d
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-02-03 00:34:36 +0900

    Update urls.py

--- compare/urls.py
+++ compare/urls.py
@@ -4,15 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload/', views.upload, name='upload'),
-    path('search/', views.search, name='search'),
-    path('request/', views.account_request, name='request'),
-    path('genes/', views.genes, name='genes'),
-    path('gene/<str:gene_name>', views.gene, name='gene'),
-    path('variants/', views.variants, name='variants'),
-    path('gene/<str:gene_name>/variant/<str:protein>', views.variant, name='variant'),
-    path('gene/<str:gene_name>/variant/<str:protein>/result', views.variant_text, name='variant_text'),
-    path('gene/<str:gene_name>/variant/<str:protein>/export', views.export, name='export'),
-    path('gene/<str:gene_name>/variant/<str:protein>/exported', views.exported, name='exported'),
-    path('gene/<str:gene_name>/variant/<str:variant_p>/history', views.history, name='history'),
+    path('export/', views.export, name='upload'),
 ]
