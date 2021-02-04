f8a22857c6f7c027f2b1db208398a3ca83108b51
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-02-04 09:27:08 +0900

    Create views.py

---
+++ html_parse/views.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import os
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from SabPadLIMS import settings
+from .functions import parse_link
+
+sep = os.path.sep
+parse_app = Blueprint('parse_app', __name__, template_folder='../templates')
+TITLE = ('파일비교 애플리케이션', '파일 내역을 비교하는 애플리케이션')
+
+
+@login_required
+def url_parse(request):
+    data = None
+    if request.method == 'POST':
+        data = parse_link(request.POST.get('link'))
+    return render_template(request, 'html_parse/index.html', {'title': TITLE, 'data': data, 'user': request.user})
