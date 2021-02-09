04c24b04bf886e049a94bab2e4da2541a9ce1e6d
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-02-09 15:29:21 +0900

    Update settings.py

--- SabPadLIMS/settings.py
+++ SabPadLIMS/settings.py
@@ -108,11 +108,7 @@ WSGI_APPLICATION = 'SabPadLIMS.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-    # 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'mysql://root:password@localhost:3306/database_db')),
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'mysql://root:password@localhost:3306/database_db')),
 }
 
 AUTH_USER_MODEL = 'accounts.User'
