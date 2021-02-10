f29052acdd9f77406d64d4f7cc1f313d6dc93dfd
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-02-11 07:44:54 +0900

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,12 +13,12 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('username', 'staff', 'specialist', 'counselor', 'scientist', 'admin')
+    list_display = ('username', 'staff', 'admin')
     list_filter = ('admin',)
     fieldsets = (
         (None, {'fields': ('username', 'email', 'password')}),
         ('Personal info', {'fields': ()}),
-        ('Permissions', {'fields': ('staff', 'specialist', 'counselor', 'scientist', 'admin')}),
+        ('Permissions', {'fields': ('staff', 'admin')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
