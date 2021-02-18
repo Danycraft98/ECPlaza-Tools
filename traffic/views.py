538718f16be537aa795f1b718c596a85b8a9228d
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-02-18 16:59:24 +0900

    Updated Traffic app.

--- traffic/views.py
+++ traffic/views.py
@@ -1,7 +1,11 @@
 import os
 
+import httplib2
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
 
 from traffic.functions import get_access_token
 
@@ -11,6 +15,25 @@ sep = os.path.sep
 
 @login_required
 def traffic(request):
+    # Create service credentials
+    # Rename your JSON key to client_secrets.json and save it to your working folder
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('ecplaza-67f2563cb042.json', ['https://www.googleapis.com/auth/analytics.readonly'])
+
+    # Create a service object
+    http = credentials.authorize(httplib2.Http())
+    service = build('analytics', 'v4', http=http, discoveryServiceUrl='https://analyticsreporting.googleapis.com/$discovery/rest')
+    response = service.reports().batchGet(body={'reportRequests': [
+        {
+            'viewId': '260668703',  # Add View ID from GA
+            'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
+            'metrics': [{'expression': 'ga:sessions'}],
+            'dimensions': [{"name": "ga:pagePath"}],  # Get Pages
+            "filtersExpression": "ga:pagePath=~products;ga:pagePath!@/translate",  # Filter by condition "containing products"
+            'orderBys': [{"fieldName": "ga:sessions", "sortOrder": "DESCENDING"}],
+            'pageSize': 100
+        }]
+    }).execute()
+    print(response)
     google_access_token = get_access_token()
     print(os.environ.get('GOOGLE_USER_ID', None))
     google_userid = os.environ.get('GOOGLE_USER_ID', None)
