44b750289b79d01c4c4a6171a1690f6c212e329b
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-02-18 15:36:45 +0900

    Updated traffic app.

--- traffic/functions.py
+++ traffic/functions.py
@@ -5,5 +5,5 @@ __all__ = ['get_access_token']
 
 def get_access_token():
     SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
-    KEY_FILEPATH = 'ecplaza-tools-34beebf3cff9.json'
+    KEY_FILEPATH = 'ecplaza-67f2563cb042.json'
     return ServiceAccountCredentials.from_json_keyfile_name(KEY_FILEPATH, SCOPE).get_access_token().access_token
